1b28e3e669d6ca923014351f475fa5e01ac4d6a3
lingdejun <342882403@qq.com>
2020-05-13 22:34:03 +0800

    update

--- src/views/booking/index.vue
+++ src/views/booking/index.vue
@@ -1,54 +1,83 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
+      <el-form :inline="true" :model="listQuery" class="demo-form-inline">
+        <el-row>
+          <el-col :span="22">
+            <el-input v-model="listQuery.title" icon="el-icon-search" placeholder="预约记录搜索" class="filter-item" @keyup.enter.native="handleFilter" />
+          </el-col>
+          <el-col :span="2">
+            <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+              Search
+            </el-button>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-form-item label="预约入口">
+            <el-select v-model="listQuery.enter">
+              <el-option label="区域一" value="shanghai" />
+              <el-option label="区域二" value="beijing" />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="接待人">
+            <el-input v-model="listQuery.receptionist" />
+          </el-form-item>
+          <el-form-item label="预约日期">
+            <el-date-picker
+              v-model="listQuery.bookingdate"
+              type="datetimerange"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              value-format="yyyy-MM-dd HH:mm:ss"
+            />
+          </el-form-item>
+          <el-form-item label="访客类型">
+            <el-select v-model="listQuery.visitortype">
+              <el-option label="区域一" value="shanghai" />
+              <el-option label="区域二" value="beijing" />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="预约状态">
+            <el-select v-model="listQuery.bookstatus">
+              <el-option label="区域一" value="shanghai" />
+              <el-option label="区域二" value="beijing" />
+            </el-select>
+          </el-form-item>
+        </el-row>
+        <el-row>
+          <el-form-item label="来访单位">
+            <el-input v-model="listQuery.visitorcompany" />
+          </el-form-item>
+          <el-form-item label="来访事由">
+            <el-select v-model="listQuery.visitreason">
+              <el-option label="区域一" value="shanghai" />
+              <el-option label="区域二" value="beijing" />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="来访人员">
+            <el-input v-model="listQuery.visitor" />
+          </el-form-item>
+          <el-form-item label="来访车辆">
+            <el-input v-model="listQuery.visitcar" />
+          </el-form-item>
+        </el-row>
+      </el-form>
+    </div>
+    <div style="padding-bottom: 10px;padding-top: 10px;background-color:#FFFFFF;border:1px solid #dfe6ec;border-top-right-radius: 10px;border-top-left-radius: 10px;">
       <el-row>
-        <el-col :span="22">
-          <el-input v-model="listQuery.title" icon="el-icon-search" placeholder="预约记录搜索" class="filter-item" @keyup.enter.native="handleFilter" />
-        </el-col>
-        <el-col :span="2">
-          <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
-            Search
+        <el-col :span="4"><img :src="lineIcon"><img :src="listIcon" style="margin-left:20px"><span style="margin-left:10px;clear: both;vertical-align: top;font-size: 25px;">结果列表</span></el-col>
+        <el-col :span="8" :offset="12" style="text-align:center">
+          <el-button v-waves class="filter-item" @click="handleFilter">
+            一周
+          </el-button>
+          <el-button v-waves class="filter-item" @click="handleFilter">
+            一月
+          </el-button>
+          <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
+            导&emsp;出
           </el-button>
         </el-col>
       </el-row>
-      <!-- <el-row>
-        <el-col :span="8">
-          <span class="filter-item">来访单位</span>
-          <el-input v-model="listQuery.title" placeholder="Title" class="filter-item" style="width: 285px;margin-left:10px" />
-        </el-col>
-        <el-col :span="8">
-          <span class="filter-item" style="margin-left:20px">来访事由</span>
-          <el-select v-model="listQuery.importance" placeholder="Imp" style="margin-left:5px" class="filter-item">
-            <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
-          </el-select>
-        </el-col>
-        <el-col :span="8" style="text-align: center;">
-          <span class="filter-item">来访人员</span>
-          <el-select v-model="listQuery.importance" placeholder="Imp" style="width: 100px;margin-left:10px" class="filter-item">
-            <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
-          </el-select>
-          <span class="filter-item" style="margin-left:20px">来访车辆</span>
-          <el-input v-model="listQuery.title" placeholder="Title" class="filter-item" style="width: 100px;margin-left:10px" />
-        </el-col>
-      </el-row> -->
-    </div>
-    <div class="filter-container">
-      <el-input v-model="listQuery.title" placeholder="Title" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-select v-model="listQuery.importance" placeholder="Imp" clearable style="width: 90px" class="filter-item">
-        <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
-      </el-select>
-      <el-select v-model="listQuery.sort" style="width: 140px" class="filter-item" @change="handleFilter">
-        <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key" />
-      </el-select>
-      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
-        Add
-      </el-button>
-      <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
-        Export
-      </el-button>
-      <el-checkbox v-model="showReviewer" class="filter-item" style="margin-left:15px;" @change="tableKey=tableKey+1">
-        reviewer
-      </el-checkbox>
     </div>
     <el-table
       :key="tableKey"
@@ -171,7 +200,8 @@ import { fetchList, fetchPv, createArticle, updateArticle } from '@/api/article'
 import waves from '@/directive/waves' // waves directive
 import { parseTime } from '@/utils'
 import Pagination from '@/components/Pagination' // secondary package based on el-pagination
-
+import listIcon from '@/icons/List_Icon_2.png'
+import lineIcon from '@/icons/Line_1.png'
 export default {
   name: 'Booking',
   components: { Pagination },
@@ -188,6 +218,8 @@ export default {
   },
   data() {
     return {
+      listIcon: listIcon,
+      lineIcon: lineIcon,
       tableKey: 0,
       list: null,
       total: 0,
@@ -195,10 +227,16 @@ export default {
       listQuery: {
         page: 1,
         limit: 20,
-        importance: undefined,
-        title: undefined,
-        type: undefined,
-        sort: '+id'
+        title: '',
+        enter: '',
+        receptionist: '',
+        bookingdate: '',
+        visitortype: '',
+        bookstatus: '',
+        visitorcompany: '',
+        visitreason: '',
+        visitor: '',
+        visitcar: ''
       },
       importanceOptions: [1, 2, 3],
       sortOptions: [{ label: 'ID Ascending', key: '+id' }, { label: 'ID Descending', key: '-id' }],
@@ -247,6 +285,7 @@ export default {
     },
     handleFilter() {
       this.listQuery.page = 1
+      console.log(this.listQuery.bookingDate)
       this.getList()
     },
     handleModifyStatus(row, status) {
